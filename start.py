b7e351dc7c15d2da2ae7363143fa60a2ae5dceb9
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-06-19 12:08:09 +0300

    Переработка файла запуска

--- start.py
+++ start.py
@@ -1,22 +1,25 @@
 from model.parser import site_connection
+from model.text_processing import list_article
 from pickle import load, dump
-morph_dict_path = r'setting\morph.rol'
-print('initialization_start')
-parser = site_connection()
-parser.search_option('Интеллектуальный анализ данных')
-print('initialization_end')
-print('parser_start')
-parser.getting_these_articles()
-print('parser_end')
-articles = parser.articles
-with open('Doc_and_test\\test.txt', 'w') as f:
-    for i, article in enumerate(articles):
-        f.write('{}\t{}\n'.format(i + 1, article.name))
-with open('Doc_and_test\\articles.rol', 'wb') as f:
-    dump(articles, f)
+from wordcloud import WordCloud
 
-# with open('index.html', 'w', encoding='utf-8') as f:
-#     f.write(site.text)
+if __name__ == "__main__":
+    morph_dict_path = r'setting\morph.rol'
+    print('initialization_start')
+    parser = site_connection()
+    parser.search_option('Интеллектуальный анализ данных')
+    print('initialization_end')
+    print('parser_start')
+    parser.getting_these_articles()
+    print('parser_end')
+    articles = list_article(parser.articles)
+    with open('Doc_and_test\\articles.rol', 'wb') as f:
+        dump(articles, f)
+
+
+    # with open('index.html', 'w', encoding='utf-8') as f:
+    #     f.write(site.text)
+
 
 
 
